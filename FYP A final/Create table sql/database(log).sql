95b2f2104984c30915ca13d38b3fdde2a4dd54d9
MarkGoh <markgoh1997@gmail.com>
2019-10-05 16:06:48 +0800

    Database Refine

--- FYP A final/Create table sql/database(log).sql
+++ FYP A final/Create table sql/database(log).sql
@@ -1,13 +1,15 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET GLOBAL time_zone = '+8:00';
 
-CREATE TABLE IF NOT EXISTS `Log` (
+CREATE TABLE IF NOT EXISTS `TimeLog` (
 `LogID` int(3) NOT NULL,
-  `LogDesc` varchar(100) NOT NULL,
-  `TicketNo` int(3) NOT NULL,
-    `EmpID` int(3) NOT NULL,
-    `SaveDate` date NOT NULL
+  `BudgetTime` time NOT NULL,
+  `Start` int(3) NOT NULL,
+    `Completed` int(3) NOT NULL,
+    `TotalTimeUsed` time NOT NULL, 
+    `Status` char(15) NOT NULL,
+    `UAT` char(6)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-ALTER TABLE `Log`
+ALTER TABLE `TimeLog`
  ADD PRIMARY KEY (`LogID`);
--- FYP A final/Create table sql/database(ticket).sql
+++ FYP A final/Create table sql/database(ticket).sql
@@ -6,18 +6,23 @@ CREATE TABLE IF NOT EXISTS `TICKET` (
 `TicketNo` int(3) NOT NULL,
   `TicketTitle` varchar(20) NOT NULL,
   `TicketDesc` varchar(50) NOT NULL,
-    `PID` int(3) NOT NULL,
+    `PID` int(3) NOT NULL REFERENCES CUSTOMER(CustID),
     `Status` varchar(10) NOT NULL,
     `DateReceived` date NOT NULL,
-    `StaffID` int(3) NOT NULL REFERENCES EMPLOYEE(EmpID)
+    `DomainName` varchar(63) NOT NULL,
+    `SourceOnline` varchar(50) NOT NULL,
+    `SourceOffline` varchar(50) NOT NULL,
+    `Warranty` char(3),
+    `Diagnosis` varchar(100),
+    `Findings` varchar(100),
+    `Others` varchar(100),
+    `Cause` varchar(100),
+    `StaffID` int(3) REFERENCES EMPLOYEE(EmpID),
+    `JobID` varchar(3)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
-
 
 ALTER TABLE `TICKET`
  ADD PRIMARY KEY (`TicketNo`);
-
 
 ALTER TABLE `TICKET`
 MODIFY `TicketNo` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
